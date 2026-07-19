aaf318239f31169f71710b2e233e70afa25ac701
Swarup2108 <gaikwadswarup21@gmail.com>
2026-07-20 02:23:38 +0530

    Use lightweight DistilBERT model

--- sentiment.py
+++ sentiment.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 
 classifier = pipeline(
     "sentiment-analysis",
-    model="cardiffnlp/twitter-roberta-base-sentiment-latest"
+    model="distilbert-base-uncased-finetuned-sst-2-english"
 )
 
 def analyze_sentiment(text):
